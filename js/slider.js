e4ff4abaeadf9815b277450428922a8302e16ab3
SimonDebray <simon.debray@devinci.fr>
2017-09-12 00:18:28 +0200

    slider & animation

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,34 @@
-/**
- * Created by debray on 08/09/2017.
+var i = 1;
 
-document.getElementById("sliderLeft").addEventListener("click", function () {
-    console.log('hello');
-});
+var lastScrollTop = 0;
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
 
-document.getElementById("sliderRight").addEventListener("scroll", function () {
-    console.log('hello');
-});
- */
+var slides = document.getElementById("slides");
+slides.addEventListener("scroll", slide);
+
+function slide(){
+    slides.removeEventListener("scroll", slide);
+    slides.setAttribute("style", "overflow-y: hidden");
+
+    var st = document.getElementById("slides").scrollTop;
+    console.log(st);
+    console.log(lastScrollTop);
+    if (st > lastScrollTop){
+        i++;
+    } else if (st < lastScrollTop){
+        i--;
+    }
+
+    if(i > 4) i = 4;
+    if(i < 1) i = 1;
+
+    document.getElementById('s'+i).scrollIntoView();
+
+    lastScrollTop = st;
+
+    setTimeout(function(){
+        slides.addEventListener("scroll", slide);
+        slides.setAttribute("style", "overflow-y: scroll");
+    }, 500);
+}
+
